acb5ec2e2fe06b0684dc86bc235e203032c5aeb1
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2021-11-06 15:42:59 +0000

    Update __init__

--- wilderness/__init__.py
+++ wilderness/__init__.py
@@ -2,4 +2,7 @@
 
 from .application import Application
 from .command import Command
+from .group import Group
 from .manpages import manpage_builder
+
+__all__ = ["Application", "Command", "Group", "manpage_builder"]
