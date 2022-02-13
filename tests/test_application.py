f7b76071a4180195d6bd1e867175bb951a0e0555
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2022-02-13 12:38:42 +0000

    clean up test code

--- tests/test_application.py
+++ tests/test_application.py
@@ -7,30 +7,34 @@ from wilderness.help import HelpCommand
 from wilderness.tester import Tester
 
 
+class TestApp(Application):
+    def __init__(self):
+        super().__init__(
+            "testapp",
+            version="0.1.0",
+            title="testapp title",
+            author="John Doe",
+            description="long description",
+            extra_sections={"test": "test section"},
+        )
+
+    def register(self):
+        self.add_argument(
+            "-q",
+            "--quiet",
+            help="be quiet",
+            action="store_true",
+            description="Run in quiet mode",
+        )
+
+
 class ApplicationTestCase(unittest.TestCase):
-    def test_application_1(self):
-        class TestApp(Application):
-            def __init__(self):
-                super().__init__(
-                    "testapp",
-                    version="0.1.0",
-                    title="testapp title",
-                    author="John Doe",
-                    description="long description",
-                    extra_sections={"test": "test section"},
-                )
+    def setUp(self):
+        self._app = TestApp()
+        self._helptext = "usage: testapp [-h] [-q] command ...\n"
 
-            def register(self):
-                self.add_argument(
-                    "-q",
-                    "--quiet",
-                    help="be quiet",
-                    action="store_true",
-                    description="Run in quiet mode",
-                )
-
-        app = TestApp()
-
+    def test_application_base(self):
+        app = self._app
         self.assertEqual(app.name, "testapp")
         self.assertEqual(app.author, "John Doe")
         self.assertEqual(app.version, "0.1.0")
@@ -38,38 +42,43 @@ class ApplicationTestCase(unittest.TestCase):
         self.assertEqual(len(app.commands), 1)
         self.assertTrue(isinstance(app.commands[0], HelpCommand))
 
-        helptext = "usage: testapp [-h] [-q] command ...\n"
-        self.assertEqual(app.format_help(), helptext)
+    def test_application_help_1(self):
+        self.assertEqual(self._app.format_help(), self._helptext)
 
+    def test_application_help_2(self):
         # Test that we get help without any arguments
-        tester = Tester(app)
+        tester = Tester(self._app)
         tester.test_application()
         self.assertEqual(tester.get_return_code(), 1)
-        self.assertEqual(tester.get_stdout(), helptext)
+        self.assertEqual(tester.get_stdout(), self._helptext)
 
+    def test_application_help_3(self):
         # Test that we get help with only -h flag
-        tester = Tester(app)
+        tester = Tester(self._app)
         tester.test_application(["-h"])
         self.assertEqual(tester.get_return_code(), 1)
-        self.assertEqual(tester.get_stdout(), helptext)
+        self.assertEqual(tester.get_stdout(), self._helptext)
 
+    def test_application_help_4(self):
         # Test that we get help with only --help
-        tester = Tester(app)
+        tester = Tester(self._app)
         tester.test_application(["--help"])
         self.assertEqual(tester.get_return_code(), 1)
-        self.assertEqual(tester.get_stdout(), helptext)
+        self.assertEqual(tester.get_stdout(), self._helptext)
 
+    def test_application_help_5(self):
         # Test that we get help with --help and other arguments
-        tester = Tester(app)
+        tester = Tester(self._app)
         tester.test_application(["-q", "--help"])
         self.assertEqual(tester.get_return_code(), 1)
-        self.assertEqual(tester.get_stdout(), helptext)
+        self.assertEqual(tester.get_stdout(), self._helptext)
 
+    def test_application_help_6(self):
         # Test that we get help when help command supplied
-        tester = Tester(app)
+        tester = Tester(self._app)
         tester.test_application(["help"])
         self.assertEqual(tester.get_return_code(), 1)
-        self.assertEqual(tester.get_stdout(), helptext)
+        self.assertEqual(tester.get_stdout(), self._helptext)
 
 
 if __name__ == "__main__":
